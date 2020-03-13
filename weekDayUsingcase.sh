21730741b4ed066f45a14783dad62d18b8bd1d37
ARATI-UPARE <aratiupare@gmail.com>
2020-03-13 10:00:14 +0530

    ADDED:WeekDay using case

---
+++ weekDayUsingcase.sh
@@ -0,0 +1,31 @@
+#! /bin/bash -x
+
+# WAP to Display print day of week  using  case statement.
+
+read -p "Enter Number of day of week " day
+
+case $day in
+		1)
+				echo Sunday
+				;;
+		2)
+      		echo Monday
+				;;
+		3)
+				echo Tuesday
+				;;
+		4)
+      		echo Wednesday
+				;;
+		5)
+      		echo Thursday
+				;;
+		6)
+				echo Friday
+				;;
+		7)
+				echo Saturday
+				;;
+esac
+
+
